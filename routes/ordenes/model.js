e42daf331f6571bfefa9a797f2043bbb3f61080b
unknown <anthonycardoze@gmail.com>
2020-11-21 23:28:18 -0500

    funcionando ordenes y linea de compra

--- routes/ordenes/model.js
+++ routes/ordenes/model.js
@@ -1,96 +1,146 @@
-const { database } = require("../../database/database");
+const { database } = require('../../database/database');
 
 const getAllOrdenes = async () => {
-  return database
-    .select("a.*", "b.name as nombre", "b.lastname as apellido")
-    .from("ordenes as a")
-    .innerJoin("usuarios as b", "a.id_user", "b.user_id")
-    .then((orden) => {
-      return orden;
-    })
-    .catch((error) => {
-      return error;
-    });
+	return database
+		.select('a.*', 'b.name as nombre', 'b.lastname as apellido')
+		.from('ordenes as a')
+		.innerJoin('usuarios as b', 'a.id_user', 'b.user_id')
+		.then((orden) => {
+			return orden;
+		})
+		.catch((error) => {
+			return error;
+		});
 };
 
 const getAllOrdenesWithPages = async (offset, limit, atrib, order) => {
-  return database
-    .select("a.*", "b.name as nombre", "b.lastname as apellido")
-    .from("ordenes as a")
-    .innerJoin("usuarios as b", "a.id_user", "b.user_id")
-    .limit(limit)
-    .offset(offset)
-    .orderBy(`${atrib}`, `${order}`)
-    .then((orden) => {
-      return orden;
-    })
-    .catch((error) => {
-      return error;
-    });
+	return database
+		.select('a.*', 'b.name as nombre', 'b.lastname as apellido')
+		.from('ordenes as a')
+		.innerJoin('usuarios as b', 'a.id_user', 'b.user_id')
+		.limit(limit)
+		.offset(offset)
+		.orderBy(`${atrib}`, `${order}`)
+		.then((orden) => {
+			return orden;
+		})
+		.catch((error) => {
+			return error;
+		});
 };
 
 const getOrdenesDataExcel = async () => {
-  return database
-    .select("a.*", "b.name as nombre", "b.lastname as apellido")
-    .from("ordenes as a")
-    .innerJoin("usuarios as b", "a.id_user", "b.user_id")
-    .then((orden) => {
-      return orden;
-    })
-    .catch((error) => {
-      return error;
-    });
+	return database
+		.select('a.*', 'b.name as nombre', 'b.lastname as apellido')
+		.from('ordenes as a')
+		.innerJoin('usuarios as b', 'a.id_user', 'b.user_id')
+		.then((orden) => {
+			return orden;
+		})
+		.catch((error) => {
+			return error;
+		});
 };
 
 const paginateQueryResults = async (
-  page,
-  limit,
-  atrib,
-  order,
-  getAll,
-  getWithPages,
+	page,
+	limit,
+	atrib,
+	order,
+	getAll,
+	getWithPages,
 ) => {
-  const offset = limit * page - limit;
-  const endIndex = page * limit;
-  const results = {};
-  const total = await getAll();
-  results.total = total.length;
+	const offset = limit * page - limit;
+	const endIndex = page * limit;
+	const results = {};
+	const total = await getAll();
+	results.total = total.length;
 
-  if (endIndex < total.length) {
-    results.next = {
-      page: page + 1,
-      limit: limit,
-    };
-  }
+	if (endIndex < total.length) {
+		results.next = {
+			page: page + 1,
+			limit: limit,
+		};
+	}
 
-  if (page > 1) {
-    results.previous = {
-      page: page,
-      limit: limit,
-    };
-  }
+	if (page > 1) {
+		results.previous = {
+			page: page,
+			limit: limit,
+		};
+	}
 
-  results.results = await getWithPages(offset, limit, atrib, order);
-  return results;
+	results.results = await getWithPages(offset, limit, atrib, order);
+	return results;
 };
 
 const getOrdenesBySearch = async (text) => {
-  return database
-    .select("a.*", "b.name as nombre", "b.lastname as apellido")
-    .from("ordenes as a")
-    .innerJoin("usuarios as b", "a.id_user", "b.user_id")
-    .where("b.name", "like", `%${text}%`)
-    .orWhere("b.lastname", "like", `%${text}%`)
-    .orWhere("a.orden_id", "like", `%${text}%`)
-    .orWhere("a.pedido_id", "like", `%${text}%`)
-    .orWhere("a.nombre_cliente", "like", `%${text}%`)
-    .orWhere("a.numero_cliente", "like", `%${text}%`)
-    .then((data) => {
-      return data;
-    })
-    .catch((err) => {
-      return err;
-    });
+	return database
+		.select('a.*', 'b.name as nombre', 'b.lastname as apellido')
+		.from('ordenes as a')
+		.innerJoin('usuarios as b', 'a.id_user', 'b.user_id')
+		.where('b.name', 'like', `%${text}%`)
+		.orWhere('b.lastname', 'like', `%${text}%`)
+		.orWhere('a.orden_id', 'like', `%${text}%`)
+		.orWhere('a.pedido_id', 'like', `%${text}%`)
+		.orWhere('a.nombre_cliente', 'like', `%${text}%`)
+		.orWhere('a.numero_cliente', 'like', `%${text}%`)
+		.then((data) => {
+			return data;
+		})
+		.catch((err) => {
+			return err;
+		});
+};
+
+const crearOrden = async (obj, id_user, fecha) => {
+	return database('ordenes')
+		.insert({
+			pedido_id: obj.id_pedido,
+			id_user: id_user,
+			fecha: fecha,
+			nombre_cliente: obj.nombre_cliente,
+			direccion_cliente: obj.direccion_cliente,
+			numero_cliente: obj.numero_cliente,
+			estatus: 1,
+		})
+		.then((orden) => {
+			return orden;
+		})
+		.catch((err) => {
+			return err;
+		});
+};
+
+const crearProducto = async (
+	pedido_id,
+	producto,
+	talla,
+	color,
+	cantidad,
+	precio,
+	id_user,
+	estatus,
+	orden_id,
+) => {
+	return database('linea_compra')
+		.insert({
+			pedido_id,
+			producto,
+			talla,
+			color,
+			cantidad,
+			precio,
+			id_user,
+			estatus,
+			orden_id,
+		})
+		.then((compra) => {
+			return compra;
+		})
+		.catch((err) => {
+			return err;
+		});
 };
 
 module.exports.getAllOrdenes = getAllOrdenes;
@@ -98,3 +148,5 @@ module.exports.getAllOrdenesWithPages = getAllOrdenesWithPages;
 module.exports.getOrdenesDataExcel = getOrdenesDataExcel;
 module.exports.getOrdenesBySearch = getOrdenesBySearch;
 module.exports.paginateQueryResults = paginateQueryResults;
+module.exports.crearOrden = crearOrden;
+module.exports.crearProducto = crearProducto;
--- routes/ordenes/ordenes.js
+++ routes/ordenes/ordenes.js
@@ -1,69 +1,111 @@
-const router = require("express").Router();
-const verify = require("../../functions/verifytoken");
+const router = require('express').Router();
+const verify = require('../../functions/verifytoken');
+const moment = require('moment');
 
 const {
-  getAllOrdenes,
-  getAllOrdenesWithPages,
-  getOrdenesDataExcel,
-  paginateQueryResults,
-  getOrdenesBySearch,
-} = require("./model.js");
+	getAllOrdenes,
+	getAllOrdenesWithPages,
+	getOrdenesDataExcel,
+	paginateQueryResults,
+	getOrdenesBySearch,
+	crearOrden,
+	crearProducto,
+} = require('./model.js');
 
-router.get("/all-ordenes", verify, async (req, res) => {
-  const page = parseInt(req.query.page);
-  const limit = parseInt(req.query.limit);
-  const atrib = req.query.atrib;
-  const order = req.query.order;
-  const excel = req.query.excel;
+router.get('/all-ordenes', verify, async (req, res) => {
+	const page = parseInt(req.query.page);
+	const limit = parseInt(req.query.limit);
+	const atrib = req.query.atrib;
+	const order = req.query.order;
+	const excel = req.query.excel;
 
-  if (excel) {
-    try {
-      const query = await getOrdenesDataExcel();
-      console.log("data de ordenes para excel ...");
-      res.status(200).json(query);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  } else {
-    try {
-      if (
-        req.query.page === undefined &&
-        req.query.limit === undefined &&
-        req.query.atrib === undefined &&
-        req.query.order === undefined
-      ) {
-        const query = await getAllOrdenes();
-        console.log("dataset de tabla de ordenes completa ...");
-        res.status(200).json(query);
-      } else {
-        const query = await paginateQueryResults(
-          page,
-          limit,
-          atrib,
-          order,
-          getAllOrdenes,
-          getAllOrdenesWithPages,
-        );
-        console.log("entregando todos los ordenes ...");
-        res.status(200).json(query);
-      }
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  }
+	if (excel) {
+		try {
+			const query = await getOrdenesDataExcel();
+			console.log('data de ordenes para excel ...');
+			res.status(200).json(query);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	} else {
+		try {
+			if (
+				req.query.page === undefined &&
+				req.query.limit === undefined &&
+				req.query.atrib === undefined &&
+				req.query.order === undefined
+			) {
+				const query = await getAllOrdenes();
+				console.log('dataset de tabla de ordenes completa ...');
+				res.status(200).json(query);
+			} else {
+				const query = await paginateQueryResults(
+					page,
+					limit,
+					atrib,
+					order,
+					getAllOrdenes,
+					getAllOrdenesWithPages,
+				);
+				console.log('entregando todos los ordenes ...');
+				res.status(200).json(query);
+			}
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
 });
 
 // search
 
-router.get("/search", verify, async (req, res) => {
-  const text = req.query.text;
-  try {
-    const query = await getOrdenesBySearch(text);
-    console.log("entregando todas las ordenes segun busqueda ...");
-    res.status(200).json(query);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+router.get('/search', verify, async (req, res) => {
+	const text = req.query.text;
+	try {
+		const query = await getOrdenesBySearch(text);
+		console.log('entregando todas las ordenes segun busqueda ...');
+		res.status(200).json(query);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+router.post('/crear', verify, async (req, res) => {
+	const orden = req.body.orden;
+	const user_id = req.user.user_id;
+	//verificar que el pedido exista y este activo
+
+	//verificar que tiene productos
+	if (orden.productos.length === 0)
+		return res.status(400).json('no hay productos para esta orden');
+
+	//guardar la orden en tabla de ordenes
+	const guardarOrden = await crearOrden(
+		orden,
+		user_id,
+		moment().format('YYYY-MM-DD'),
+	);
+	if (!guardarOrden) res.status(400).json('error al guardar la orden');
+	console.log('orden creada..');
+
+	//crear los productos en tabla de linea de compra
+	orden.productos.forEach(async (producto) => {
+		const guardar = await crearProducto(
+			orden.id_pedido,
+			producto.descripcion,
+			producto.talla,
+			producto.color,
+			producto.cantidad,
+			producto.precio,
+			user_id,
+			1,
+			guardarOrden[0],
+		);
+		if (!guardar)
+			return res.status(400).json(`error al guardar el producto ${producto}`);
+		console.log('producto creado...');
+	});
+
+	res.status(200).json('orden creada exitosamente');
 });
 
 module.exports = router;
