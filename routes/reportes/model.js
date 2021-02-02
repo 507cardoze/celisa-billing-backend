f13839340ee5a5d3de59b8d0bbec31bebdb6abdd
unknown <anthonycardoze@gmail.com>
2021-02-01 23:23:54 -0500

    reportes de proveedores y por vendedores

--- routes/reportes/model.js
+++ routes/reportes/model.js
@@ -1,23 +1,193 @@
 const { database } = require("../../database/database");
 
+const sumar = (array, attrib) => {
+  return parseFloat(
+    array
+      .reduce((a, b) => {
+        return a + b[attrib];
+      }, 0)
+      .toFixed(2),
+  );
+};
+
 const getAllOrdenesByFechaWithCompra = async (desde, hasta) => {
   return database
-    .raw(
-      `SELECT a.pedido_id ,a.orden_id, a.nombre_cliente, a.direccion_cliente as direccion, CONCAT(d.name,' ', d.lastname) as vendedor , ROUND(SUM(b.precio),2) AS ventas , IFNULL(ROUND(pago.pagos,2),0) as pagos, ROUND(SUM(b.precio),2) - IFNULL(ROUND(pago.pagos,2),0) as saldo
-      FROM ordenes AS a
-      LEFT JOIN linea_compra AS b ON b.orden_id = a.orden_id
-      LEFT JOIN (SELECT orden_id, SUM(cantidad) AS pagos FROM linea_pago WHERE estatus = 1 GROUP BY orden_id) AS pago ON pago.orden_id = a.orden_id
-      INNER JOIN usuarios as d ON a.id_user = d.user_id
-      WHERE a.estatus = 1 AND b.estatus = 1 AND a.fecha BETWEEN '${desde}' AND '${hasta}'
-      GROUP BY a.orden_id
-      ORDER BY a.orden_id DESC`,
+    .select(
+      "a.pedido_id",
+      "a.orden_id",
+      "a.nombre_cliente",
+      "a.direccion_cliente as direccion",
+      database.raw(`CONCAT(d.name,' ', d.lastname) as vendedor`),
+      database.raw(`SUM(ventas.ventas)  AS ventas`),
+      database.raw(`IFNULL(ROUND(pago.pagos,2),0) as pagos`),
+      database.raw(
+        `SUM(ventas.ventas) - IFNULL(ROUND(pago.pagos,2),0) as saldo`,
+      ),
     )
+    .from("ordenes AS a")
+    .leftJoin(
+      database
+        .select(
+          "orden_id",
+          database.raw(`ROUND(SUM(cantidad * precio),2) as ventas`),
+        )
+        .from("linea_compra")
+        .where("estatus", "=", 1)
+        .groupBy("orden_id")
+        .as("ventas"),
+      "ventas.orden_id",
+      "a.orden_id",
+    )
+    .leftJoin(
+      database
+        .select("orden_id", database.raw(`ROUND(SUM(cantidad),2) as pagos`))
+        .from("linea_pago")
+        .where("estatus", "=", 1)
+        .groupBy("orden_id")
+        .as("pago"),
+      "pago.orden_id",
+      "a.orden_id",
+    )
+    .innerJoin("usuarios as d", "a.id_user", "d.user_id")
+    .where("a.estatus", "=", 1)
+    .whereBetween("a.fecha", [desde, hasta])
+    .groupBy("a.orden_id")
+    .orderBy("a.orden_id", "DESC")
     .then((ordenes) => {
-      return ordenes[0];
+      return ordenes;
     })
     .catch((error) => {
       return error;
     });
 };
 
+const getAllProveedoresVentas = async (desde, hasta) => {
+  return database
+    .select(
+      "a.proveedor",
+      database.raw(`SUM(IFNULL(b.cantidad,0)) as productos`),
+      database.raw(`IFNULL(ROUND(SUM(b.cantidad * b.precio),2),0) as ventas`),
+    )
+    .from("proveedores as a")
+    .leftJoin("linea_compra as b", "b.proveedor_id", "a.proveedor_id")
+    .innerJoin("ordenes as c", "b.orden_id", "c.orden_id")
+    .whereBetween("c.fecha", [desde, hasta])
+    .andWhere("a.estatus", "=", 1)
+    .andWhere("b.estatus", "=", 1)
+    .groupBy("a.proveedor")
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+const getAllProveedoresVentasTotal = async () => {
+  return database
+    .select(
+      "a.proveedor",
+      database.raw(`SUM(IFNULL(b.cantidad,0)) as productos`),
+      database.raw(`IFNULL(ROUND(SUM(b.cantidad * b.precio),2),0) as ventas`),
+    )
+    .from("proveedores as a")
+    .leftJoin("linea_compra as b", "b.proveedor_id", "a.proveedor_id")
+    .andWhere("a.estatus", "=", 1)
+    .andWhere("b.estatus", "=", 1)
+    .groupBy("a.proveedor")
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+const getAllProductosSinProveedor = async () => {
+  return database
+    .select("a.linea_id", "a.proveedor_id")
+    .from("linea_compra as a")
+    .where("a.estatus", "=", 1)
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+const getAllVendedoresConVentas = async (desde, hasta) => {
+  return database
+    .select(
+      "a.name",
+      "a.lastname",
+      database.raw(`COUNT(b.orden_id) as ventas`),
+      database.raw(`SUM(pago.ventasTotal) as ventas_total`),
+    )
+    .from("usuarios as a")
+    .leftJoin("ordenes as b", "b.id_user", "a.user_id")
+    .leftJoin(
+      database
+        .select(
+          "orden_id",
+          database.raw("ROUND(SUM(cantidad * precio),2) as ventasTotal"),
+        )
+        .from("linea_compra")
+        .where("estatus", "=", 1)
+        .groupBy("orden_id")
+        .as("pago"),
+      "pago.orden_id",
+      "b.orden_id",
+    )
+    .whereBetween("b.fecha", [desde, hasta])
+    .andWhere("a.estado", "=", 1)
+    .andWhere("b.estatus", "=", 1)
+    .groupBy("a.user_id")
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+const getAllVendedoresConVentasTotal = async () => {
+  return database
+    .select(
+      "a.name",
+      "a.lastname",
+      database.raw(`COUNT(b.orden_id) as ventas`),
+      database.raw(`SUM(pago.ventasTotal) as ventas_total`),
+    )
+    .from("usuarios as a")
+    .leftJoin("ordenes as b", "b.id_user", "a.user_id")
+    .leftJoin(
+      database
+        .select(
+          "orden_id",
+          database.raw("ROUND(SUM(cantidad * precio),2) as ventasTotal"),
+        )
+        .from("linea_compra")
+        .where("estatus", "=", 1)
+        .groupBy("orden_id")
+        .as("pago"),
+      "pago.orden_id",
+      "b.orden_id",
+    )
+    .where("a.estado", "=", 1)
+    .where("b.estatus", "=", 1)
+    .groupBy("a.user_id")
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
 module.exports.getAllOrdenesByFechaWithCompra = getAllOrdenesByFechaWithCompra;
+module.exports.sumar = sumar;
+module.exports.getAllProveedoresVentas = getAllProveedoresVentas;
+module.exports.getAllProveedoresVentasTotal = getAllProveedoresVentasTotal;
+module.exports.getAllProductosSinProveedor = getAllProductosSinProveedor;
+module.exports.getAllVendedoresConVentas = getAllVendedoresConVentas;
+module.exports.getAllVendedoresConVentasTotal = getAllVendedoresConVentasTotal;
--- routes/reportes/reportes.js
+++ routes/reportes/reportes.js
@@ -2,44 +2,92 @@ const router = require("express").Router();
 const verify = require("../../functions/verifytoken");
 const moment = require("moment");
 
-const { getAllOrdenesByFechaWithCompra } = require("./model");
+const {
+  getAllOrdenesByFechaWithCompra,
+  sumar,
+  getAllProveedoresVentas,
+  getAllProveedoresVentasTotal,
+  getAllProductosSinProveedor,
+  getAllVendedoresConVentas,
+  getAllVendedoresConVentasTotal,
+} = require("./model");
+
+const { getAllProveedores } = require("../proveedores/model");
 
 router.get("/", async (req, res) => {
   const desde = req.query.desde;
   const hasta = req.query.hasta;
-  const fecha = moment().format("DD MM YYYY hh:mm");
-
-  const ordenesFiltradas = await getAllOrdenesByFechaWithCompra(desde, hasta);
-
-  function sumar(array, attrib) {
-    return array.reduce((a, b) => {
-      return a + b[attrib];
-    }, 0);
-  }
-
+  const fecha = moment().format("DD-MM-YYYY hh:mm a");
   const reporte = {
-    desde: desde,
-    hasta: hasta,
+    desde,
+    hasta,
     reporteGenerado: fecha,
-    ventasTotales: parseFloat(sumar(ordenesFiltradas, "ventas").toFixed(2)),
-    pagosTotales: parseFloat(sumar(ordenesFiltradas, "pagos").toFixed(2)),
-    saldosTotales: parseFloat(sumar(ordenesFiltradas, "saldo").toFixed(2)),
-    desglose: ordenesFiltradas,
   };
 
-  res.status(200).json(reporte);
-});
+  try {
+    const ordenesFiltradas = await getAllOrdenesByFechaWithCompra(desde, hasta);
+    reporte.ventasTotales = sumar(ordenesFiltradas, "ventas");
+    reporte.pagosTotales = sumar(ordenesFiltradas, "pagos");
+    reporte.saldosTotales = sumar(ordenesFiltradas, "saldo");
+    reporte.desglose = ordenesFiltradas;
 
-router.get("/ranking-clientes", async (req, res) => {
-  res.status(200).json("ranking clientes");
+    res.status(200).json(reporte);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({});
+  }
 });
 
 router.get("/ranking-proveedores", async (req, res) => {
-  res.status(200).json("ranking proveedores");
+  const desde = req.query.desde;
+  const hasta = req.query.hasta;
+  const fecha = moment().format("DD-MM-YYYY hh:mm a");
+  const reporte = {
+    desde,
+    hasta,
+    reporteGenerado: fecha,
+  };
+  try {
+    const proveedoresData = await getAllProveedoresVentas(desde, hasta);
+    const proveedoresTotal = await getAllProveedoresVentasTotal();
+    const productosSinProveedor = await getAllProductosSinProveedor();
+    const TodosProveedores = await getAllProveedores();
+
+    reporte.productosTotal = productosSinProveedor.length;
+    reporte.productosSinproveedor = productosSinProveedor.filter(
+      (producto) => producto.proveedor_id === null,
+    ).length;
+    reporte.porFecha = proveedoresData;
+    reporte.total = proveedoresTotal;
+    reporte.proveedoresList = TodosProveedores;
+
+    res.status(200).json(reporte);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({});
+  }
 });
 
 router.get("/ranking-vendedores", async (req, res) => {
-  res.status(200).json("ranking proveedores");
+  const desde = req.query.desde;
+  const hasta = req.query.hasta;
+  const fecha = moment().format("DD-MM-YYYY hh:mm a");
+  const reporte = {
+    desde,
+    hasta,
+    reporteGenerado: fecha,
+  };
+
+  try {
+    const usuariosConVentas = await getAllVendedoresConVentas(desde, hasta);
+    const usuariosConVentasTotal = await getAllVendedoresConVentasTotal();
+    reporte.usuariosConVenta = usuariosConVentas;
+    reporte.usuariosConVentaTotal = usuariosConVentasTotal;
+    res.status(200).json(reporte);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({});
+  }
 });
 
 module.exports = router;
