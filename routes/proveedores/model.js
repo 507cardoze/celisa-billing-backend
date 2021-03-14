d9288c80ac0a757ef359aed0ea29c21c8cb1ac3e
unknown <anthonycardoze@gmail.com>
2021-03-14 11:06:55 -0500

    agregando la rutas para CRUD de proveedores

--- routes/proveedores/model.js
+++ routes/proveedores/model.js
@@ -1,66 +1,128 @@
-const { database } = require('../../database/database');
+const { database } = require("../../database/database");
 
 const getAllProveedores = async () => {
-	return database
-		.select('*')
-		.from('proveedores')
-		.where('estatus', '=', 1)
-		.then((res) => {
-			return res;
-		})
-		.catch((error) => {
-			return error;
-		});
+  return database
+    .select("*")
+    .from("proveedores")
+    .where("estatus", "=", 1)
+    .then((res) => {
+      return res;
+    })
+    .catch((error) => {
+      return error;
+    });
 };
 
 const getAllProveedoresWithPages = async (offset, limit, atrib, order) => {
-	return database
-		.select('*')
-		.from('proveedores')
-		.where('estatus', '=', 1)
-		.limit(limit)
-		.offset(offset)
-		.orderBy(`${atrib}`, `${order}`)
-		.then((res) => {
-			return res;
-		})
-		.catch((error) => {
-			return error;
-		});
+  return database
+    .select("*")
+    .from("proveedores")
+    .where("estatus", "=", 1)
+    .limit(limit)
+    .offset(offset)
+    .orderBy(`${atrib}`, `${order}`)
+    .then((res) => {
+      return res;
+    })
+    .catch((error) => {
+      return error;
+    });
 };
 
 const paginateQueryResults = async (
-	page,
-	limit,
-	atrib,
-	order,
-	getAll,
-	getWithPages,
+  page,
+  limit,
+  atrib,
+  order,
+  getAll,
+  getWithPages,
 ) => {
-	const offset = limit * page - limit;
-	const endIndex = page * limit;
-	const results = {};
-	const total = await getAll();
-	results.total = total.length;
+  const offset = limit * page - limit;
+  const endIndex = page * limit;
+  const results = {};
+  const total = await getAll();
+  results.total = total.length;
 
-	if (endIndex < total.length) {
-		results.next = {
-			page: page + 1,
-			limit: limit,
-		};
-	}
+  if (endIndex < total.length) {
+    results.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
 
-	if (page > 1) {
-		results.previous = {
-			page: page,
-			limit: limit,
-		};
-	}
+  if (page > 1) {
+    results.previous = {
+      page: page,
+      limit: limit,
+    };
+  }
 
-	results.results = await getWithPages(offset, limit, atrib, order);
-	return results;
+  results.results = await getWithPages(offset, limit, atrib, order);
+  return results;
+};
+
+const getProveedorBySearch = async (text) => {
+  return database
+    .select("*")
+    .from("proveedores")
+    .where("estatus", "=", 1)
+    .andWhere("proveedor", "like", `%${text}%`)
+    .orderBy("proveedor", "ASC")
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+const getProveedorDetails = async (proveedor_id) => {
+  return database
+    .select("*")
+    .from("proveedores")
+    .where("estatus", "=", 1)
+    .andWhere("proveedor_id", "=", proveedor_id)
+    .orderBy("proveedor", "ASC")
+    .then((proveedores) => {
+      return proveedores;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+const updateProveedorDetails = async (obj) => {
+  return database("proveedores")
+    .update({
+      proveedor: obj.proveedor,
+    })
+    .where("proveedor_id", "=", obj.proveedor_id)
+    .then((proveedores) => {
+      return proveedores;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+const crearProveedor = async (obj) => {
+  return database("proveedores")
+    .insert({
+      proveedor: obj.proveedor,
+      estatus: 1,
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      return err;
+    });
 };
 
 module.exports.getAllProveedores = getAllProveedores;
 module.exports.getAllProveedoresWithPages = getAllProveedoresWithPages;
 module.exports.paginateQueryResults = paginateQueryResults;
+module.exports.getProveedorBySearch = getProveedorBySearch;
+module.exports.getProveedorDetails = getProveedorDetails;
+module.exports.updateProveedorDetails = updateProveedorDetails;
+module.exports.crearProveedor = crearProveedor;
--- routes/proveedores/proveedores.js
+++ routes/proveedores/proveedores.js
@@ -1,39 +1,97 @@
-const router = require('express').Router();
-const verify = require('../../functions/verifytoken');
+const router = require("express").Router();
+const verify = require("../../functions/verifytoken");
 
 const {
-	getAllProveedores,
-	getAllProveedoresWithPages,
-	paginateQueryResults,
-} = require('./model.js');
+  getAllProveedores,
+  getAllProveedoresWithPages,
+  paginateQueryResults,
+  getProveedorBySearch,
+  getProveedorDetails,
+  updateProveedorDetails,
+  crearProveedor,
+} = require("./model.js");
 
-router.get('/all-proveedores', verify, async (req, res) => {
-	const page = parseInt(req.query.page);
-	const limit = parseInt(req.query.limit);
-	const atrib = req.query.atrib;
-	const order = req.query.order;
+router.get("/all-proveedores", verify, async (req, res) => {
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const atrib = req.query.atrib;
+  const order = req.query.order;
 
-	if (
-		req.query.page === undefined &&
-		req.query.limit === undefined &&
-		req.query.atrib === undefined &&
-		req.query.order === undefined
-	) {
-		const query = await getAllProveedores();
-		console.log('dataset de tabla de proveedores completa ...');
-		res.status(200).json(query);
-	} else {
-		const query = await paginateQueryResults(
-			page,
-			limit,
-			atrib,
-			order,
-			getAllProveedores,
-			getAllProveedoresWithPages,
-		);
-		console.log('entregando todos los proveedores ...');
-		res.status(200).json(query);
-	}
+  if (
+    req.query.page === undefined &&
+    req.query.limit === undefined &&
+    req.query.atrib === undefined &&
+    req.query.order === undefined
+  ) {
+    const query = await getAllProveedores();
+    console.log("dataset de tabla de proveedores completa ...");
+    res.status(200).json(query);
+  } else {
+    const query = await paginateQueryResults(
+      page,
+      limit,
+      atrib,
+      order,
+      getAllProveedores,
+      getAllProveedoresWithPages,
+    );
+    console.log("entregando todos los proveedores ...");
+    res.status(200).json(query);
+  }
+});
+
+router.get("/proveedorSearch", verify, async (req, res) => {
+  const texto = req.query.texto;
+
+  try {
+    const query = await getProveedorBySearch(texto);
+    console.log("buscando proveedores");
+    console.log(query);
+    res.status(200).json(query);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+router.get("/proveedorDetails", verify, async (req, res) => {
+  const proveedor_id = req.query.proveedor_id;
+
+  try {
+    const query = await getProveedorDetails(proveedor_id);
+    console.log("sacando detalles de proveedor");
+    res.status(200).json(query);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+router.put("/proveedorDetails", verify, async (req, res) => {
+  const objdata = req.body;
+  try {
+    const validacion = await getProveedorDetails(objdata.proveedor_id);
+    if (validacion.length === 0)
+      return res.status(400).json("proveedor existe");
+    await updateProveedorDetails(objdata);
+    console.log("modificando detalles de proveedor");
+    res.status(200).json(`Detalles Actualizados.`);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+router.post("/proveedorDetails", verify, async (req, res) => {
+  const objdata = req.body;
+  try {
+    await crearProveedor(objdata);
+    console.log("Proveedor Creado");
+    res.status(200).json(`Proveedor Creado.`);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
