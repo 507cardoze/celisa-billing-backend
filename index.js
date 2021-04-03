6978c2a62b567c1d7c304899994bc61d9f0e005b
Anthony Salvattore Cardoze Agrio <anthonycardoze@gmail.com>
2021-04-02 23:39:07 -0500

    agregando middlewares para incrementar seguridad y debugging

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 //Middlewares
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(helmet());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms"),
+);
 app.use(express.json({ limit: "5mb" }));
 app.use(
   express.urlencoded({
@@ -12,6 +19,7 @@ app.use(
   }),
 );
 app.use(cors());
+app.use(compression());
 
 // importando rutas
 const auth = require("./routes/auth/auth");
