80a69cfc7029b13618f139861f945391d14d06a5
Anthony Salvattore Cardoze Agrio <anthonycardoze@gmail.com>
2021-04-03 00:01:59 -0500

    prueba para ver si se resuelve problema de crash en production

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(helmet());
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms"),
-);
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms"),
+  );
+}
+
 app.use(express.json({ limit: "5mb" }));
 app.use(
   express.urlencoded({
